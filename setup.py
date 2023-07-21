059bf824203ab267b59da350e40c3ac6cfd013a9
quillahava <voyt.to.work@ya.ru>
2023-07-21 20:58:28 +0300

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cli_utilite",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["requests", "packaging", "argparse"],
+    entry_points={
+        "console_scripts": [
+            "cli_utilite = cli_utilite.__main__:main",
+        ],
+    },
+    author="quillahava",
+    author_email="voyt.to.work@yandex.ru",
+    description="CLI utility for comparing lists of binary packages",
+    url="https://github.com/quillahava/cli_utilite",
+)
