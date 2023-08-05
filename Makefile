a189a0643e99c9ce5729b97af0ffc0a7bbeb5828
quillahava <voyt.to.work@yandex.ru>
2023-08-05 11:40:57 +0300

    installation script configured

--- Makefile
+++ Makefile
@@ -5,12 +5,8 @@ shell:
 
 # Install cli_utilite into virtual environment
 install: 
-	python setup.py build
 	python setup.py install
 
-# Clean up build artifacts
-clear:
-	rm -rf build
 
 # Uninstall the utility (remove it from the system)
 uninstall:
--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ setup(
     name="altbrains",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["requests", "packaging", "argparse", "pydantic"],
+    install_requires=[
+        "requests",
+        "pydantic",
+    ],
     entry_points={
         "console_scripts": [
-            "altbrains = altbrains.__main__:main",
+            "altbrains=altbrains.main:main",
         ],
     },
-    author="quillahava",
-    author_email="voyt.to.work@yandex.ru",
-    description="CLI utility for comparing lists of binary packages",
-    url="https://github.com/quillahava/cli_utilite",
 )
